cfe5580d375126751d37ad6dcbc5372f675e491e
Alexandr Varygin <otz-job@mail.ru>
2020-07-08 21:38:52 +0300

    created dist

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,47 @@
+import getSpecialDescription from '../app';
+
+test('Проверка на совпадение данных', () => {
+  const character = {
+    name: "Лучник",
+    type: "Bowman",
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: "Двойной выстрел",
+        icon: "http://...",
+        description: "Двойной выстрел наносит двойной урон",
+      },
+      {
+        id: 9,
+        name: "Нокаутирующий удар",
+        icon: "http://...",
+        // <- обратите внимание, описание "засекречено"
+      },
+    ],
+  };
+
+  const expected = [
+    {
+      id: 8,
+      name: "Двойной выстрел",
+      icon: "http://...",
+      description: "Двойной выстрел наносит двойной урон",
+    },
+    {
+      id: 9,
+      name: "Нокаутирующий удар",
+      icon: "http://...",
+      description: "Описание недоступно",
+    },
+  ];
+
+  const received = getSpecialDescription(character);
+
+  expect(received).toEqual(expected);
+
+});
+
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-
+export default function getSpecialDescription(obj) {
+  const result = [];
+  for (const elem of obj.special) {
+    const {
+      id,
+      name,
+      icon,
+      description = 'Описание недоступно',
+    } = elem;
+    result.push({
+      id,
+      name,
+      icon,
+      description,
+    });
+  }
+  return result;
+}
